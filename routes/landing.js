ff61da4030ea9ff65dac98d5d9fa0059bbab698a
Kyle Much <kylemuch@gmail.com>
2016-09-02 10:45:48 -0700

    reverted to Veronicas last

--- routes/landing.js
+++ routes/landing.js
@@ -3,19 +3,11 @@ const router = express.Router();
 const knex = require('../db/knex');
 const Modules = require('../db/modules');
 
-<<<<<<< HEAD
-
-=======
->>>>>>> bd4eef688cadb436770a6bac355ed5da396066ac
 router.get('/', function(req, res, next) {
   if(req.user) {
         res.render('landing', { company: "Fortified Health" });
   } else {
-<<<<<<< HEAD
-    res.redirect('/login', { error: "You need to validate through LinkedIn" });
-=======
     res.redirect('/');
->>>>>>> bd4eef688cadb436770a6bac355ed5da396066ac
   }
 });
 
